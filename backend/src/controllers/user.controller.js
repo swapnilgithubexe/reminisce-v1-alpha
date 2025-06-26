e381bcb39a50822b8256acf635c0702440f64f2d
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-06-26 12:31:12 +0530

    feature: friend request model & send friend request controller function

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import { User } from "../models/User.js";
 import logger from "../utils/logger.js";
 
@@ -23,7 +24,7 @@ export const getRecommendedUsers = async (req, res) => {
 
     res.status(200).json(recommendedUsers);
   } catch (error) {
-    logger.error(error.message);
+    logger.error("Error in get recommended users controller function: ", error.message);
   }
 }
 
@@ -34,6 +35,69 @@ export const getMyFriends = async (req, res) => {
     logger.info(`Friends fetched for ${user.fullName}`);
 
     res.status(200).json(user.friends);
+  } catch (error) {
+    logger.error("Error in get recommended users controller function: ", error.message);
+  }
+}
+
+// Friend Request
+export const sendFriendRequest = async (req, res) => {
+  try {
+    const myId = req.user.id;
+
+    const { id: recipientId } = req.params;
+
+    //!preventing sending req to myself
+    if (myId === recipientId) {
+      logger.error(`Oops, you can't send a friend request to yourself ${req.user.fullName}`);
+      return res.status(400).json({
+        message: "You can't send a friend request to yourself"
+      })
+    };
+
+    const recipient = await User.findById(recipientId);
+    if (!recipient) {
+      logger.error("Recipient not found");
+      return res.status(404).json({
+        message: "Recipient not found"
+      })
+    };
+
+    //! checking if user is already a friend
+    if (!recipient.friends.includes(myId)) {
+      logger.error("You are already friends with this user");
+      return res.status(400).json({
+        message: "You are already friends with this user"
+      })
+    }
+
+    //! check if a frnd req exists
+    const existingRequest = await FriendRequest.findOne({
+      //! or is used here to check if either of us has sent a friend request or not, is there any instance where me is sender and other person is the reciever or vice versa
+      $or: [
+        {
+          sender: myId, recipient: recipientId
+        },
+        {
+          sender: recipientId, recipient: myId
+        }
+      ]
+    })
+
+    if (existingRequest) {
+      logger.error(`A friend request alreay exists between ${req.user.fullName} and ${recipient.fullName}`);
+      return res.status(400).json({
+        message: "A friend request alreay exists between you and this recipient"
+      })
+    }
+    const friendRequest = new FriendRequest({
+      sender: myId,
+      recipient: recipientId
+    })
+    await friendRequest.save();
+    logger.info(`A friend request has been sent between ${req.user.fullName} and ${recipient.fullName}`);
+
+    res.status(201).json(friendRequest);
   } catch (error) {
     logger.error(error.message);
   }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { protectedRoute } from '../middlewares/auth.middleware.js';
-import { getRecommendedUsers } from '../controllers/user.controller.js';
+import { getMyFriends, getRecommendedUsers, sendFriendRequest } from '../controllers/user.controller.js';
 
 export const userRouter = express.Router();
 
-//! will be applied on all routes
+//! will be applied on all routes a.k.a global middleware
 userRouter.use(protectedRoute)
 
 userRouter.get("/", getRecommendedUsers);
+userRouter.get('/friends', getMyFriends);
 
+userRouter.get("/friend-request/:id", sendFriendRequest);
