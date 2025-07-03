00a838d89fcb27ea6aa65c8e6e14a75158a22a0c
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-07-03 16:05:33 +0530

    pages template setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-red-300 h-screen">App</div>;
+  return (
+    <div className="bg-red-200 h-screen" data-theme="luxury">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/OnBoardingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OnBoardingPage = () => {
+  return <div>OnBoardingPage</div>;
+};
+
+export default OnBoardingPage;
