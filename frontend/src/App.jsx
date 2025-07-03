c7a08772213388935132421daba77b1aaa7182c0
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-07-04 00:26:21 +0530

    tanstack-query config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import { Routes, Route } from "react-router";
+import { Toaster } from "react-hot-toast";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import NotificationPage from "./pages/NotificationPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
 
 const App = () => {
   return (
-    <div className="bg-red-200 h-screen" data-theme="luxury">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className="bg-red-100 h-screen" data-theme="luxury">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+      </Routes>
+
+      <Toaster />
     </div>
   );
 };
