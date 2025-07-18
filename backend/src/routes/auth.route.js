75875086075b33375a6b6bc9f31ca73661fd9710
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-07-18 15:31:27 +0530

    removed login limiter

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,7 +6,7 @@ import { loginLimiter, registerLimiter } from "../middlewares/rateLimiter.middle
 export const authRouter = express.Router();
 
 authRouter.post("/signup", registerLimiter, signUp);
-authRouter.post("/login", loginLimiter, login);
+authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 
 authRouter.post("/onboarding", protectedRoute, onBoard);
