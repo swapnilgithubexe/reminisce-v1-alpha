5d5ca7f940498e5fc5d6c2c7e0798f8b62ebd679
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-07-18 21:55:08 +0530

    production: changed authUser stale time

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -2,16 +2,15 @@ import { getAuthUser } from '../lib/api';
 import { useQuery } from '@tanstack/react-query';
 
 const useAuthUser = () => {
-  //* const isTokenPresent = localStorage.getItem("token")
+  const isTokenPresent = localStorage.getItem("token")
   //! react query setup
   //! custom hook is created and the code from app.js is transferred here to make it look cleaner and make it readable
   const authUser = useQuery({
     queryKey: ["authUser"],
     queryFn: getAuthUser,
     retry: false,    //! auth check only once
-    //!enabled: !!isTokenPresent, //! only fetch when token is there
-
-    staleTime: 0 //! quick refetch
+    enabled: !!isTokenPresent, //! only fetch when token is there
+    staleTime: 1000 * 60 * 5 //! refetch time
   });
 
   return { isLoading: authUser.isLoading, authUser: authUser.data?.user, refetch: authUser.refetch }
