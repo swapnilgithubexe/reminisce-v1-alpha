3806070d7c4aec5af5a6811e13e29b0d65582870
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-07-24 14:59:00 +0530

    logs: error logs update

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,16 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { login } from '../lib/api';
 import { toast } from "react-hot-toast";
+import { useNavigate } from 'react-router';
 
 const useLogin = () => {
   //! query client instance
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const { isPending, mutate, error } = useMutation({
     mutationFn: login,
     onSuccess: () => {
-      toast.success("Log in successful!")
-      queryClient.invalidateQueries({ queryKey: ["authUser"] })
+      toast.success("Log in successful!");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+      navigate("/");
     },
     onError: (error) => {
       toast.error(error.response?.data?.message || error.response?.data)
